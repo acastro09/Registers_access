4daf7565d842a726d0ab1a490d8450f6617c1b6c
Albert Castro <alberthcasqui@gmail.com>
2026-09-11 16:36:39 -0600

    fixing bugs, adding git ingnore and readme

--- Reg32bits.c
+++ Reg32bits.c
@@ -9,21 +9,21 @@ typedef enum {
     REG_OK = 0,
     REG_ERR_VALUE_OUT_OF_RANGE,
     REG_ERR_NULL
-} set_status_t;
+} reg_status_t;
 
 typedef struct {
-    set_status_t status;
+    reg_status_t status;
     uint32_t reg;
-} speed_struct_t;
+} reg_result_t;
 
-speed_struct_t reg_set_speed(uint32_t reg, uint32_t speed) {
+reg_result_t reg_set_speed(uint32_t reg, uint32_t speed) {
     if (speed>SPEED_MASK){
-        return (speed_struct_t){.status = REG_ERR_VALUE_OUT_OF_RANGE, .reg = reg};
+        return (reg_result_t){.status = REG_ERR_VALUE_OUT_OF_RANGE, .reg = reg};
     }
     uint32_t newreg = reg & ~(SPEED_MASK<<SPEED_SHIFT);
     speed = (speed & SPEED_MASK)<<SPEED_SHIFT;
-    newreg = reg | speed;
-    return (speed_struct_t) {.status = REG_OK, .reg = newreg};
+    newreg = newreg | speed;
+    return (reg_result_t) {.status = REG_OK, .reg = newreg};
 }
 
 uint32_t reg_get_speed(uint32_t reg) {
@@ -38,7 +38,7 @@ int main(void) {
     uint32_t reg = 0x00000544;
     uint32_t speed = reg_get_speed(reg);
     printf("La velocidad es: %X\n", speed);
-    speed_struct_t r = reg_set_speed(reg, 5);
+    reg_result_t r = reg_set_speed(reg, 5);
     if (r.status == REG_OK){
         reg = r.reg;
         printf("el nuevo registro es: 0x%08X\n", r.reg);
@@ -56,6 +56,10 @@ int main(void) {
     }
     speed = reg_get_speed(r.reg);
     printf("La velocidad es: %X\n", speed);
+    uint32_t antes = reg;
+    reg_result_t malo = reg_set_speed(reg, 20);
+    assert(malo.status == REG_ERR_VALUE_OUT_OF_RANGE);
+    assert(malo.reg ==antes);
     assert((reg_set_speed(0xFFFFFFFF, 5)).reg==0xFFFFFFF5);
     assert(reg_get_speed((reg_set_speed(0xFFFFFFFF, 5)).reg)==5);
     assert(((reg_set_speed(0x554, 5).reg) & ~0xFu)==(0x554 & ~0xFu));
