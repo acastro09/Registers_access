c4e25c93b2b59918e09d7031ec9193d66edeb555
Albert Castro <alberthcasqui@gmail.com>
2026-09-14 11:57:26 -0600

    Modifying speed to comply with all bits

--- test_Reg32bits.c
+++ test_Reg32bits.c
@@ -4,31 +4,65 @@
 
 
 
-static void test_speed_get (void){
+static void test_bits_get (void){
     uint32_t out=0xDEADBEEF;
     assert((reg_get_bits(0x00000554u, "speed", &out)==REG_OK));
     assert(out==0x4u);
-    printf ("PASS: get speed\n");
-}
-
-
-static void test_speed_set (void){
-    uint32_t out=0xDEADBEEF;
-    assert(reg_get_bits((reg_set_bits(0xFFFFFFFF,"speed", 0X5u)).reg,"speed", &out)==REG_OK);
+    assert((reg_get_bits(0x00000554u, "width", &out)==REG_OK));
     assert(out==0x5u);
-    printf ("PASS: speed was set.\n");
-    assert(reg_set_bits(0xFFFFFFFF,"speed", 5).status==REG_OK);
-    printf("PASS: status is OK on speed update\n");
-    assert(((reg_set_bits(0x554,"speed", 5).reg) & ~0xFu)==(0x554 & ~0xFu));
-    printf ("PASS: no other bits affected when updating speed\n");
+    assert((reg_get_bits(0x00000554u, "enable", &out)==REG_OK));
+    assert(out==0x1u);
+    assert((reg_get_bits(0x00000554u, "margin", &out)==REG_OK));
+    assert(out==0x2u);
+    printf ("PASS: get bits\n");
 }
 
-static void test_out_of_range_speed (void){
+
+static void test_bits_set (void){
+    uint32_t out=0xDEADBEEF;
+    assert(reg_get_bits((reg_set_bits(0xFFFFFFFF,"speed", 0x5u)).reg,"speed", &out)==REG_OK);
+    assert(out==0x5u);
+    assert(reg_get_bits((reg_set_bits(0xFFFFFFFF,"width", 0x5u)).reg,"width", &out)==REG_OK);
+    assert(out==0x5u);
+    assert(reg_get_bits((reg_set_bits(0xFFFFFFFF,"enable", 0x1u)).reg,"enable", &out)==REG_OK);
+    assert(out==0x1u);
+    assert(reg_get_bits((reg_set_bits(0xFFFFFFFF,"margin", 0x2u)).reg,"margin", &out)==REG_OK);
+    assert(out==0x2u);
+    printf ("PASS: bits were set.\n");
+    uint32_t reg = 0x00000000;
+    assert(reg_set_bits(reg,"speed", 5).status==REG_OK);
+    assert(reg_set_bits(reg,"width", 5).status==REG_OK);
+    assert(reg_set_bits(reg,"enable", 1).status==REG_OK);
+    assert(reg_set_bits(reg,"margin", 2).status==REG_OK);
+    reg= reg_set_bits(reg,"speed", 5).reg;
+    reg= reg_set_bits(reg,"width", 5).reg;
+    reg= reg_set_bits(reg,"enable", 1).reg;
+    reg= reg_set_bits(reg,"margin", 2).reg;
+    assert (reg==0x00000555u);
+    printf("PASS: status is OK on speed update\n");
+    reg = 0x00000000;
+    assert(((reg_set_bits(reg,"speed", 5).reg) & ~0xFu)==(0x005 & ~0xFu));
+    assert(((reg_set_bits(reg,"width", 5).reg) & ~0xF0u)==(0x050 & ~0xF0u));
+    assert(((reg_set_bits(reg,"enable", 1).reg) & ~0x100u)==(0x100 & ~0x100u));
+    assert(((reg_set_bits(reg,"margin", 2).reg) & ~0x600u)==(0x400 & ~0x600u));
+    printf ("PASS: no other bits affected when updating bits\n");
+}
+
+static void test_out_of_range_bits (void){
     uint32_t out=0xDEADBEEF;
     assert(reg_get_bits((reg_set_bits(0xFFFFFFF5,"speed", 20)).reg,"speed", &out)==REG_OK);
     assert(out==0x5u);
-    printf ("PASS: speed was NOT set.\n");
+    assert(reg_get_bits((reg_set_bits(0xFFFFFF5F,"width", 20)).reg,"width", &out)==REG_OK);
+    assert(out==0x5u);
+    assert(reg_get_bits((reg_set_bits(0xFFFFFFF5,"enable", 4)).reg,"enable", &out)==REG_OK);
+    assert(out==0x1u);
+    assert(reg_get_bits((reg_set_bits(0xFFFFFFF5,"margin", 8)).reg,"margin", &out)==REG_OK);
+    assert(out==0x3u);
+    printf ("PASS: bits were NOT set.\n");
     assert((reg_set_bits(0xFFFFFFFF, "speed", 20)).status==REG_ERR_VALUE_OUT_OF_RANGE);
+    assert((reg_set_bits(0xFFFFFFFF, "width", 20)).status==REG_ERR_VALUE_OUT_OF_RANGE);
+    assert((reg_set_bits(0xFFFFFFFF, "enable", 20)).status==REG_ERR_VALUE_OUT_OF_RANGE);
+    assert((reg_set_bits(0xFFFFFFFF, "margin", 20)).status==REG_ERR_VALUE_OUT_OF_RANGE);
     printf("PASS: Not affecting other bits when modifying speed\n");
 }
 
@@ -80,9 +114,9 @@ static void test_invalid_null_out_get(void){
 
 
 int main(void) {
-    test_speed_get();
-    test_speed_set();
-    test_out_of_range_speed();
+    test_bits_get();
+    test_bits_set();
+    test_out_of_range_bits();
     test_boundaries();
     test_invalid_name_set();
     test_invalid_null_name_get();
