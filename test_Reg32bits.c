2d7f2923c7f4ac836928f2c20708ed65274ce56d
Albert Castro <alberthcasqui@gmail.com>
2026-09-13 23:14:32 -0600

    Updating tests to comply with new format

--- test_Reg32bits.c
+++ test_Reg32bits.c
@@ -2,35 +2,41 @@
 #include "Reg32bits.h"
 #include <assert.h>
 
+uint32_t out;
+
 static void test_speed_get (void){
-    assert((reg_get_speed(0x00000554u)==0x4u));
+    assert((reg_get_bits(0x00000554u, "speed", &out)==REG_OK));
+    assert(out==0x4u);
     printf ("PASS: get speed\n");
 }
 
 
 static void test_speed_set (void){
-    assert(reg_get_speed((reg_set_speed(0xFFFFFFFF, 5)).reg)==5);
+    assert(reg_get_bits((reg_set_bits(0xFFFFFFFF,"speed", 0X5u)).reg,"speed", &out)==REG_OK);
+    assert(out==0x5u);
     printf ("PASS: speed was set.\n");
-    assert(reg_set_speed(0xFFFFFFFF, 5).status==REG_OK);
-    printf("PASS: status is OK\n");
-    assert(((reg_set_speed(0x554, 5).reg) & ~0xFu)==(0x554 & ~0xFu));
-    printf ("PASS: no other bits affected\n");
+    assert(reg_set_bits(0xFFFFFFFF,"speed", 5).status==REG_OK);
+    printf("PASS: status is OK on speed update\n");
+    assert(((reg_set_bits(0x554,"speed", 5).reg) & ~0xFu)==(0x554 & ~0xFu));
+    printf ("PASS: no other bits affected when updating speed\n");
 
 }
 
 static void test_out_of_range_speed (void){
-    assert(reg_get_speed((reg_set_speed(0xFFFFFFF5, 20)).reg)==5);
+    assert(reg_get_bits((reg_set_bits(0xFFFFFFF5,"speed", 20)).reg,"speed", &out)==REG_OK);
+    assert(out==0x5u);
     printf ("PASS: speed was NOT set.\n");
-    assert((reg_set_speed(0xFFFFFFFF, 20)).status==REG_ERR_VALUE_OUT_OF_RANGE);
-    printf("PASS: Not affecting other bits\n");
+    assert((reg_set_bits(0xFFFFFFFF, "speed", 20)).status==REG_ERR_VALUE_OUT_OF_RANGE);
+    printf("PASS: Not affecting other bits when modifying speed\n");
 
 }
 
 static void test_boundaries(void){
-    assert(reg_set_speed(0x554,15).status==REG_OK);
-    assert(reg_set_speed(0x554,16).status!=REG_OK);
-    assert(reg_get_speed((reg_set_speed(0xFFFFFFFF, 5)).reg)==5);
-    printf("PASS: Boundaries well defined\n");
+    assert(reg_set_bits(0x554,"speed",15).status==REG_OK);
+    assert(reg_set_bits(0x554,"speed",16).status!=REG_OK);
+    assert(reg_get_bits((reg_set_bits(0xFFFFFFFF,"speed", 5)).reg,"speed", &out)==REG_OK);
+    assert(out==0x5u);
+    printf("PASS: Speed boundaries well defined\n");
 }
 
 
